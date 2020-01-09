f48f1636853bebe5ab9e7f22363a012dc95f27b8
Serhii Litvinenko <slitvinenko@gbsfo.com>
2020-01-09 18:46:25 +0200

    1.2.5: ET Whitespace filter extension

--- base/src/main/java/com/core/base/util/Extenstions.kt
+++ base/src/main/java/com/core/base/util/Extenstions.kt
@@ -9,6 +9,9 @@ import android.os.Build
 import android.os.Bundle
 import android.os.VibrationEffect
 import android.os.Vibrator
+import android.text.InputFilter
+import android.text.SpannableString
+import android.text.Spanned
 import android.view.View
 import android.view.ViewTreeObserver
 import android.text.TextUtils
@@ -173,4 +176,38 @@ fun Fragment.vibratePhone(duration: Long) {
     } else {
         vibrator.vibrate(duration)
     }
+}
+
+fun EditText.setWhiteSpaceFilter() = run {
+    this.filters = arrayOf(object : InputFilter {
+        override fun filter(
+            source: CharSequence,
+            start: Int,
+            end: Int,
+            dest: Spanned,
+            dstart: Int,
+            dend: Int
+        ): CharSequence? {
+            var keepOriginal = true
+            val sb = StringBuilder(end - start)
+            for (i in start until end) {
+                val c = source[i]
+                if (isCharAllowed(c))
+                    sb.append(c) else keepOriginal = false
+            }
+            return if (keepOriginal) null else {
+                if (source is Spanned) {
+                    val sp = SpannableString(sb)
+                    TextUtils.copySpansFrom(source, start, sb.length, null, sp, 0)
+                    sp
+                } else {
+                    sb
+                }
+            }
+        }
+
+        private fun isCharAllowed(c: Char): Boolean {
+            return Character.isLetterOrDigit(c)
+        }
+    })
 }

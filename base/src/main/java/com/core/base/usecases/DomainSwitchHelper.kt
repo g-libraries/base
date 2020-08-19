d4d1c9b81a896b2dcf034f8965d33b22dafd94de
Roman Kokosh <rkokosh@gbsfo.com>
2020-08-19 16:21:26 +0300

    DomainSwitchHelper added

---
+++ base/src/main/java/com/core/base/usecases/DomainSwitchHelper.kt
@@ -0,0 +1,56 @@
+package com.core.base.usecases
+
+import android.app.Activity
+import android.content.SharedPreferences
+import android.os.Handler
+import android.view.View
+import android.widget.Toast
+
+class DomainSwitchHelper(
+    val sharedPreferences: SharedPreferences,
+    val activity: Activity,
+    val build: Boolean
+) {
+
+    val MODE = "mode"
+
+    fun initSwitch(view: View) {
+        var isOk = true
+        var counter = 0
+        val handler = Handler()
+        view.setOnClickListener {
+            counter++
+
+            handler.postDelayed({
+                isOk = false
+            }, 1000)
+
+            if (!isOk)
+                counter = 0
+
+            handler.removeCallbacksAndMessages(null)
+
+            if (counter > 10) {
+                setMode(!getMode())
+
+                Toast.makeText(
+                    activity,
+                    if (getMode()) "Production. Reload the app" else "Development. Reload the app",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                counter = 0
+            }
+        }
+    }
+
+
+    fun setMode(isProd: Boolean) {
+        sharedPreferences.edit().putBoolean(MODE, isProd).apply()
+    }
+
+    fun getMode(): Boolean {
+        return sharedPreferences.getBoolean(MODE, !build)
+    }
+
+}
